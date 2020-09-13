373e68f1424f1c14e6bc2bb0d2ee2d71e4912b19
iamkaustubh1019 <kaustubh1019@gmail.com>
2020-09-13 13:21:48 +0530

    Updates

---
+++ precache-manifest.a8ebe5c466115d08569b6f6da207e5e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14630fc57c934db4ceff56cff67fe705",
+    "url": "/detector/index.html"
+  },
+  {
+    "revision": "575524d2907acb7f5683",
+    "url": "/detector/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5022ba37c4a25ad1dfe9",
+    "url": "/detector/static/js/2.e3c8b425.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/detector/static/js/2.e3c8b425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575524d2907acb7f5683",
+    "url": "/detector/static/js/main.5d8a1cfd.chunk.js"
+  },
+  {
+    "revision": "7fc0e39e0baeb7f815b2",
+    "url": "/detector/static/js/runtime-main.51373416.js"
+  }
+]);
